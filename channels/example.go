cdbee8540b3a2658e7115638a0ae9265f69d2bff
Sjur Millidahl <smillida@cisco.com>
2024-06-24 15:10:53 +0200

    channels

--- channels/example.go
+++ channels/example.go
@@ -1,20 +1,19 @@
 package channels
 
 import(
-	"time"
 	"fmt"
 )
 
-func Fn(s string, c chan string) {
-	c <- s
+func Fn(c chan string) {
+	c <- "Hello"
+	c <- "World"
+	close(c)
 }
 
 func Run() {
 	ch := make(chan string)
-	go Fn("Hello", ch)
-	go Fn("World", ch)
-	x, y := <-ch, <-ch
-	close(ch)
-	time.Sleep(100 * time.Millisecond)
-	fmt.Printf("Channel receive: %s %s", x,y)
+    go Fn(ch)
+	for x := range ch {
+		fmt.Printf("%s <- | ", x)
+	}
 }
